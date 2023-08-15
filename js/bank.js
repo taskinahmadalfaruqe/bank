84a47403e112e03bbacc05032e4464c3c3d61e11
TASKIN AHMAD AL FARUQE <119452412+taskinahmadalfaruqe@users.noreply.github.com>
2023-08-15 23:51:39 +0600

    Add Clear Value Text

--- js/bank.js
+++ js/bank.js
@@ -4,21 +4,21 @@ document.getElementById('btn-deposit').addEventListener('click',function(){
     // STEP 02: GET INPUT VALUE AND CHECK TYPE OF VALUE 
     const inputValue = document.getElementById('deposit-ammonut');
     const value = inputValue.value;
-    // console.log(value, typeof value);
+    console.log(value, typeof value);
 
 
     // STEP 03:  CONVERT VALUE STRING TO NUMBER;
     const numberValue=parseFloat(value);
-    // console.log(numberValue, typeof numberValue);
+    console.log(numberValue, typeof numberValue);
 
 
     // STEP 04: GET DEPOSIT NUMBER USING INNER TEXT;
     const previousDeposit= document.getElementById('deposit');
     const  previousValue= previousDeposit.innerText;
     let preValueNumber= parseFloat(previousValue); //CONVERT STRING TO FLOAT NUMBER
-    // console.log(previousValue);
+    console.log(previousValue);
 
-    
+
     // STEP 05: SET THE SUM OF THE PREVIOUS DEPOSIT VALUE AND NEW ADD VALUE
     let depositSum = numberValue + preValueNumber;
     previousDeposit.innerText=depositSum;
@@ -28,13 +28,16 @@ document.getElementById('btn-deposit').addEventListener('click',function(){
     const previousCurrentBalance =document.getElementById('balance');
     const previousCurrentBalanceString =previousCurrentBalance.innerText;
     const previousCurrentBalanceNumber = parseFloat(previousCurrentBalanceString); //CONVERT STRING TO FLOAT NUMBER
-    // console.log(previousCurrentBalanceNumber, typeof previousCurrentBalanceNumber);
+    console.log(previousCurrentBalanceNumber, typeof previousCurrentBalanceNumber);
 
 
     // STEP 07: SET NEW CURRENT BALANCE
     const currentBalance= numberValue + previousCurrentBalanceNumber;
     previousCurrentBalance.innerText =currentBalance;
-    // let newBalance = currentBalance;
-    // console.log(newBalance);
-   
+
+    let newBalance = currentBalance;
+    console.log(newBalance);
+
+    // STEP 08: CLEAR THE DEPOSIT FIELD
+    inputValue.value='';
 })
